49dce6f632fce687f4e6c91830189d0253e88930
Imran Fazilov <ifazilov3584@bths.edu>
2023-05-01 11:46:07 -0400

    done

--- src/MainGUIWindow.java
+++ src/MainGUIWindow.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class MainGUIWindow {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainGUIWindow extends JFrame implements ActionListener {
+    private JPanel mainPanel;
+    private JTextArea pressTheButtonTextArea;
+    private JTextArea textArea1;
+    private JButton pressMeButton;
+
+    public MainGUIWindow() {
+        createUIComponents();
+    }
+
+    private void createUIComponents() {
+        setContentPane(mainPanel);
+        setTitle("Test GUI");
+        setSize(300, 400);
+        setLocation(450, 100);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        pressMeButton.addActionListener(this);
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        textArea1.append("Congratulations!");
+    }
 }
